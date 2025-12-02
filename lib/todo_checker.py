3fc36111aaa91e7da9aec8cd93d7b38e97481da7
Mitchell <mitch.lewsey@gmail.com>
2025-12-02 15:40:38 +0000

    refactored check_notes for test 2

--- lib/todo_checker.py
+++ lib/todo_checker.py
@@ -1,2 +1,2 @@
 def check_notes(notes):
-    return False
+    return '#TODO' in notes
--- tests/test_todo_checker.py
+++ tests/test_todo_checker.py
@@ -9,3 +9,12 @@ def test_given_empty_string_returns_False():
     actual = check_notes("")
     expected = False
     assert actual == expected
+
+"""
+Given a string with '#TODO' 
+It returns True
+"""
+def test_given_string_with_hashtag_TODO_returns_True():
+    actual = check_notes("#TODO finish the challenge")
+    expected = True
+    assert actual == expected
